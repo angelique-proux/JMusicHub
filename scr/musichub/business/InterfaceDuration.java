530e752f3fa0de165641a42f7df3020609f7ff3c
Manelle <60315534+manellenr@users.noreply.github.com>
2020-12-30 19:48:24 +0100

    fini

--- scr/musichub/business/InterfaceDuration.java
+++ scr/musichub/business/InterfaceDuration.java
@@ -14,5 +14,9 @@ package musichub.business;
 
 
 public interface InterfaceDuration {
+	/*
+	* It defines the signature of the method getDuration which must be implemented in the classes built from this prototype
+	*/
+	
 	public int getDuration();
 }
